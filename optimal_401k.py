7b75256aca661866ce9b42f5619e8d8380d25e60
0xDECEA5ED <0xDECEA5ED@users.noreply.github.com>
2022-01-19 15:57:23 -0600

    add lazy calculation and effort value

--- optimal_401k.py
+++ optimal_401k.py
@@ -1,6 +1,6 @@
 # Globals
-SALARY = 78750       # Your yearly gross salary
-CUR_BAL = 20500      # Approximate current value of your 401k, if any
+SALARY = 90000       # Your yearly gross salary
+CUR_BAL = 25000      # Approximate current value of your 401k, if any
 YEARLY_MAX = 20500   # Maximum amount in dollars to contribute to 401k, before tax
 MATCH_PERCENT = 0.25 # Percentage of each paycheck matched. By default this is configured for "Employer matches 25% of the first 6% contributed"
 MATCH_FIRST = 0.06   # Maximum percentage of check to match
@@ -15,21 +15,25 @@ def do_year(principal, rate, vals):
     return principal
 
 l = list()
+lz = list()
+
 remaining = YEARLY_MAX
 for month in range(0,12):
     val = int(SALARY/12) if remaining > int(SALARY/12) else remaining if remaining < int(SALARY/12) and remaining > 0 else 0
     remaining -= val
-    l.insert(0,val)
+    l.insert(0, round(val, 2))
+    lz.insert(0, round(YEARLY_MAX/12), 2)
 
-max = do_year(CUR_BAL, INTEREST_RATE, l)
+lazy = round(do_year(CUR_BAL, INTEREST_RATE, lz), 2)
+max = round(do_year(CUR_BAL, INTEREST_RATE, l), 2)
 cont = True
 while cont:
-    for i in range(len(l)-1,0,-1):
+    for i in range(len(l)-1, 0, -1):
         if l[i] > 0 and l[i-1] <= int(SALARY/12):
             cont = False
             l[i] = l[i] - STEP
             l[i-1] = l[i-1] + STEP
-            x = do_year(CUR_BAL, INTEREST_RATE, l)
+            x = round(do_year(CUR_BAL, INTEREST_RATE, l), 2)
             if x > max:
                 max = x
                 cont = True
@@ -38,4 +42,8 @@ while cont:
                 l[i] = l[i] + STEP
                 l[i-1] = l[i-1] - STEP
 
-print("Found best set:",max,l)
+print("Lazy Contribution Schedule:", lz)
+print("Lazy Year End Balance:", lazy)
+print("Optimal Contribution Schedule:", l)
+print("Optimal Year End Balance:", max)
+print("Effort Value:",round(max-lazy, 2))
